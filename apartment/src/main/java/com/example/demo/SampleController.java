3534bb9d7dd43f5956b4c3b542e2a10870c01bda
daiki-24 <daiki.eguchi@DESKTOP-59CJK11>
2024-05-17 17:52:13 +0900

    削除機能の追加

--- apartment/src/main/java/com/example/demo/SampleController.java
+++ apartment/src/main/java/com/example/demo/SampleController.java
@@ -5,20 +5,57 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.data.ApartmentDataInterface;
 import com.example.demo.data.ApartmentDataService;
+import com.example.demo.model.Apartment;
 
 @RestController
 public class SampleController {
 	@Autowired
 	ApartmentDataService service;
 	
+	// 一覧取得
 	@GetMapping("/apartment")
 	@CrossOrigin
 	public List<? extends ApartmentDataInterface> apartment() {
 		System.out.println("apartment/index");
 		return service.getAll();
+	}
+
+	// 情報取得
+	@GetMapping("/apartment/{id}")
+	@CrossOrigin
+	public ApartmentDataInterface apartmentById(@PathVariable int id) {
+		System.out.println("apartment/findById");
+		return service.getById(id);
+	}
+	
+	// 追加
+	@PostMapping("/apartment/add")
+	@CrossOrigin
+	public int add(@RequestBody Apartment apartment) {
+		System.out.println("apartment/add(post)");
+		return service.add(apartment);
+	}
+
+	// 更新
+	@PostMapping("/apartment/update")
+	@CrossOrigin
+	public int update(@RequestBody Apartment apartment) {
+		System.out.println("apartment/update(post)");
+		return service.add(apartment);
+	}
+
+	// 削除
+	@PostMapping("/apartment/delete")
+	@CrossOrigin
+	public void delete(@RequestBody Apartment apartment) {
+		System.out.println("apartment/delete(post)");
+		service.delete(apartment);
 	}
 }
--- apartment/src/main/java/com/example/demo/data/ApartmentDataService.java
+++ apartment/src/main/java/com/example/demo/data/ApartmentDataService.java
@@ -17,4 +17,25 @@ public class ApartmentDataService {
 		System.out.println("Service:getAll");
 		return repository.findAll();
 	}
+	
+	public Apartment getById(int id) {
+		System.out.println("Service:getById id[" + id + "]");
+		return repository.findById(id).orElse(null);
+	}
+
+	public int add(ApartmentDataInterface item) {
+		System.out.println("Service:add [" + item + "]");
+		if (item instanceof Apartment) {
+			Apartment savedItem = repository.saveAndFlush((Apartment)item);
+			return savedItem.getId();
+		}
+		return 0;
+	}
+	
+	public void delete(ApartmentDataInterface item) {
+		System.out.println("Service:delete[" + item + "]");
+		if (item instanceof Apartment) {
+			repository.delete((Apartment) item);
+		}
+	}
 }
